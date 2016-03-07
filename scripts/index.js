7a06d730b7734d412b42cf0a886de80c02520c89
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-07 15:30:06 -0600

    Using Date object

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,19 @@
 var currentAge = parseFloat(prompt('What is your current age?'));
 var retireAge = parseFloat(prompt('At what age would you like to retire?'));
-var yearsUntilRetire = retireAge - retireAge;
-var currentYear = Date.UTC(year); 
-var retireDate = currentYear + yearsUntilRetire;
+var yearsUntilRetire = retireAge - currentAge; 
+
+
+var currentTime = new Date();
+var year = currentTime.getFullYear();
+
+
+var retireDate = year + yearsUntilRetire;
 
 document.write('What is your current age? ' + currentAge);
+document.write('<br>');
 document.write('At what age would you like to retire? ' + retireAge);
+document.write('<br>');
 document.write('You have '+yearsUntilRetire+ 'years left until you can retire.');
-document.write('Its '+currentYear+ ' so you can retire in '+retireDate+'.');
+document.write('<br>');
+document.write('Its '+year+ ' so you can retire in '+retireDate+'.');
+
