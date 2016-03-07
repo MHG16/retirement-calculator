e06b5792098cdc61139f3a3bdc87da523672aeb9
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-07 15:52:30 -0600

    Adventurer mode complete

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,16 @@ var year = currentTime.getFullYear();
 
 var retireDate = year + yearsUntilRetire;
 
+if (yearsUntilRetire  >= 0) {
 document.write('What is your current age? ' + currentAge);
 document.write('<br>');
 document.write('At what age would you like to retire? ' + retireAge);
 document.write('<br>');
-document.write('You have '+yearsUntilRetire+ 'years left until you can retire.');
+document.write('You have '+yearsUntilRetire+ ' years left until you can retire.');
 document.write('<br>');
 document.write('Its '+year+ ' so you can retire in '+retireDate+'.');
+}
 
+else {
+	document.write('You can already retire');
+}
